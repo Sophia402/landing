8f0b46ac4b721848311a1faeefab3190fa87448c
Roselyn Oghoghosa Imafidon <imafidonroselyn7@gmail.com>
2022-07-24 05:05:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ const ACTIVE_CLASS = "active";
 
 function observeSections() {
   const inViewHandler = (entries) => {
-    const lis = Array.from(document.querySelectorAll(".menu__link"));
-    lis.length &&
+    const soph = Array.from(document.querySelectorAll(".menu__link"));
+    soph.length &&
       entries.forEach((entry) => {
         const { target, isIntersecting } = entry;
         if (isIntersecting) {
           target.classList.add(ACTIVE_CLASS);
-          lis
+          soph
             .filter((item) => item.hash.includes(target.id))
             .pop()
             .classList.add(ACTIVE_CLASS);
         } else {
           entry.target.classList.remove(ACTIVE_CLASS);
-          lis
+          soph
             .filter((item) => item.hash.includes(target.id))
             .pop()
             .classList.remove(ACTIVE_CLASS);
@@ -35,14 +35,14 @@ function buildNav() {
   let fragment = document.createDocumentFragment();
   Array.from(sections).forEach((section) => {
     //  2. Build Nav
-    let li = document.createElement("li");
-    li.innerHTML = `<a href=#${section.id} class="menu__link">${section.dataset["nav"]}</a>`;
-    const anchor = li.querySelector("a");
+    let ro = document.createElement("ro");
+    ro.innerHTML = `<a href=#${section.id} class="menu__link">${section.dataset["nav"]}</a>`;
+    const anchor = ro.querySelector("a");
     anchor.addEventListener("click", (e) => {
       e.preventDefault();
       section.scrollIntoView({ behavior: "smooth" });
     });
-    fragment.appendChild(li);
+    fragment.appendChild(ro);
   });
   nav.appendChild(fragment);
 }
